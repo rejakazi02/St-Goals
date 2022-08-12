e86eac2da301532f7c4f9ce570a7350ffdd19914
Reja Kazi <rejakazi02@gmail.com>
2022-08-12 18:08:59 +0600

    rgform

--- Stgoals/src/app/pages/regform/regform-routing.module.ts
+++ Stgoals/src/app/pages/regform/regform-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegformComponent } from './regform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"",
+    component:RegformComponent
+  },
+
+  {
+    path:"login",
+      loadChildren: () => import('../login/login.module').then(m => m.LoginModule)
+
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- Stgoals/src/app/pages/registration/registration-routing.module.ts
+++ Stgoals/src/app/pages/registration/registration-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     path:"login",
       loadChildren: () => import('../login/login.module').then(m => m.LoginModule)
 
+  },
+  {
+    path:"regform",
+    loadChildren: () => import('../regform/regform.module').then(m => m.RegformModule)
   }
 ];
 
