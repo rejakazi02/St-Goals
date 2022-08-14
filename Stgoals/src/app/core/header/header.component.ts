97b0564288319a64d605870929af49fecd610a61
Reja Kazi <rejakazi02@gmail.com>
2022-08-14 10:26:24 +0600

    done

--- Stgoals/src/app/core/header/header.component.ts
+++ Stgoals/src/app/core/header/header.component.ts
@@ -10,14 +10,49 @@ import { NavigationEnd, Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+headerHide=true;
+
+
 
   constructor(
-
-  ) { }
+    
+    private router:Router
+    
+    ) { }
 
   ngOnInit(): void {
-
+    this.headerHideControle();
+  
+    
   }
+
+headerHideControle(){
+
+// int render section 
+
+if(this.router.url=='/login' || this.router.url=='/otpform' || this.router.url=='/userlogin' || this.router.url=='/registration' || this.router.url=='/regform' || this.router.url=='/addressform'){
+     this.headerHide=false;
+  }
+  else
+  {
+    this.headerHide=true;
+  }
+ 
+ 
+
+  // nav int section 
+
+  this.router.events.subscribe(()=>{
+    if(this.router.url=='/login' || this.router.url=='/otpform' || this.router.url=='/userlogin' || this.router.url=='/registration' || this.router.url=='/regform' || this.router.url=='/addressform'){
+      this.headerHide=false;
+   }
+   else
+   {
+     this.headerHide=true;
+   }
+  })
+}
+
 
 
 }
--- Stgoals/src/app/shared/components/footer/footer.component.ts
+++ Stgoals/src/app/shared/components/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  footerHide=true;
+
+
+  constructor(
+    private router:Router) {  }
 
   ngOnInit(): void {
+    this.footerHideControle();
   }
 
+  
+footerHideControle(){
+
+  // int render section 
+  
+  if(this.router.url=='/login' || this.router.url=='/otpform' || this.router.url=='/userlogin' || this.router.url=='/registration' || this.router.url=='/regform' || this.router.url=='/addressform'){
+       this.footerHide=false;
+    }
+    else
+    {
+      this.footerHide=true;
+    }
+   
+   
+  
+    // nav int section 
+  
+    this.router.events.subscribe(()=>{
+      if(this.router.url=='/login' || this.router.url=='/otpform' || this.router.url=='/userlogin' || this.router.url=='/registration' || this.router.url=='/regform' || this.router.url=='/addressform'){
+        this.footerHide=false;
+     }
+     else
+     {
+       this.footerHide=true;
+     }
+    })
+  }
 }
