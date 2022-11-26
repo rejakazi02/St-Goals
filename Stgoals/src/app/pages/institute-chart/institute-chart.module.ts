c7337529293119eae95ed430f7ce4ee64bb278ab
Reja Kazi <rejakazi02@gmail.com>
2022-11-26 17:07:24 +0600

    Addd chart

--- Stgoals/src/app/pages/institute-chart/institute-chart.module.ts
+++ Stgoals/src/app/pages/institute-chart/institute-chart.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { InstituteChartRoutingModule } from './institute-chart-routing.module';
 import { InstituteChartComponent } from './institute-chart.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [InstituteChartComponent],
   imports: [
     CommonModule,
-    InstituteChartRoutingModule
+    InstituteChartRoutingModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatIconModule,
   ]
 })
 export class InstituteChartModule { }
