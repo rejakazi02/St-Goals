ab26d1508d7b1c60814d6013200feb10b0f6241d
Reja Kazi <rejakazi02@gmail.com>
2022-11-26 18:51:02 +0600

    Addd

--- Stgoals/src/app/pages/details-institute/details-institute.component.ts
+++ Stgoals/src/app/pages/details-institute/details-institute.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { InstituteListService } from 'src/app/services/institute-list.service';
 
 @Component({
   selector: 'app-details-institute',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsInstituteComponent implements OnInit {
 
-  constructor() { }
+
+  instSlug:any;
+  getInstiData:any;
+  userRole: string;
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private instService: InstituteListService,
+  ) { }
 
   ngOnInit(): void {
+    // update data
+    this.activateRoute.paramMap.subscribe((param) => {
+      this.instSlug = param.get('id');
+      console.log('param', this.instSlug )
+      if (this.instSlug) {
+        this.getInstiInfo(this.instSlug);
+      } 
+    });
+  }
+
+
+  
+  getInstiInfo(slug: any) {
+    this.instService.getInstiInfo(slug).subscribe((result) => {
+      this.getInstiData = result;
+     console.log('this.getUpdateData', this.getInstiData)
+      // this.test = this.getUpdateData?.student;
+      // console.log('vvvv', this.test);
+      
+      // this.classData();
+      // this.setFormData();
+    });
   }
 
 }
